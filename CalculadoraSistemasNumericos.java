a99b594943936b8d8bc696b21916656fc961d051
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2024-05-11 02:58:17 -0400

    feature: crea el metodo que convierte un numero a un sistema numerico

--- CalculadoraSistemasNumericos.java
+++ CalculadoraSistemasNumericos.java
@@ -3,4 +3,8 @@ public class CalculadoraSistemasNumericos {
         int numeroDecimal = Integer.parseInt(numero, base);
         return numeroDecimal;
     }
+    public String convertirDecimalSistemaNumerico(int numeroDecimal, int base) {
+        String numero = Integer.toString(numeroDecimal, base);
+        return numero;
+    }
 }

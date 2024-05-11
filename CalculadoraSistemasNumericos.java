22155c5358c9893639670ff83efa9ab371099472
Richixs <ricardo.ramirez@jala.university>
2024-05-11 02:05:51 -0400

    feat: metodo que convierte de sistama numerico base n a decimal

--- CalculadoraSistemasNumericos.java
+++ CalculadoraSistemasNumericos.java
@@ -1,3 +1,6 @@
 public class CalculadoraSistemasNumericos {
-    
+    public int convertirSistemaNumericoDecimal(String numero, int base) {
+        int numeroDecimal = Integer.parseInt(numero, base);
+        return numeroDecimal;
+    }
 }
